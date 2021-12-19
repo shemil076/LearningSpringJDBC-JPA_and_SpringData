a4ea818cf503136a0d8ee3796309779d3d377f27
Pramuditha Karunarathna <shemil076@gmail.com>
2021-12-19 21:46:36 +0530

    Defining person Entity

--- src/main/java/com/database/databasedemo/entity/Person.java
+++ src/main/java/com/database/databasedemo/entity/Person.java
@@ -1,18 +1,29 @@
 package com.database.databasedemo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
     private int id;
     private String name;
     private String location;
-
+    private Date birthDate;
 
 
     public Person(){
 
     }
 
+    public Person( String name, String location, Date birthDate) {
+        this.name = name;
+        this.location = location;
+        this.birthDate = birthDate;
+    }
 
     public Person(int id, String name, String location, Date birthDate) {
         this.id = id;
@@ -52,7 +63,6 @@ public class Person {
         this.birthDate = birthDate;
     }
 
-    private Date birthDate;
 
     @Override
     public String toString() {

e140fb8612973268f6d665c63393c04f12a282f7
Pramuditha Karunarathna <shemil076@gmail.com>
2021-12-19 12:15:40 +0530

    Implement findAll person Spring JDBC Query Method

---
+++ src/main/java/com/database/databasedemo/jdbc/PersonJDBCDao.java
@@ -0,0 +1,21 @@
+package com.database.databasedemo.jdbc;
+
+import com.database.databasedemo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJDBCDao {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Person> findAll() {
+       return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Person.class));
+
+    }
+}
